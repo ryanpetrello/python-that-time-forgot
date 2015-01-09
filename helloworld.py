bcd3630456a4094574adebec7bd5dbd88fc751d2
Ryan Petrello <lists@ryanpetrello.com>
2015-01-09 14:12:10 -0500

    Add some discussion topics and a working Apache + Python 1.2 CGI script

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+import sys
+
+print "Content-type: text/html"
+print
+print "<pre>"
+print "<strong>Python %s</strong>" % sys.version
+print "<strong>Hello, World!</strong>"
+print "</pre>"
